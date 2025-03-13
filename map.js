7ba3b365d4c836ba2ecd49d788e5445cd7b69d64
MiraiSuenagi <136078520+MiraiSuenagi@users.noreply.github.com>
2025-03-13 17:11:47 +0500

    Update map.js

--- map.js
+++ map.js
@@ -19,6 +19,8 @@ function loadSchools(year) {
     let filteredData = schoolData.filter(school => school.properties.completed == year);
 
     filteredData.forEach(school => {
+        console.log(school.properties); // Проверяем, какие данные есть
+
         var schoolIcon = L.icon({
             iconUrl: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
             iconSize: [20, 20]
@@ -38,6 +40,7 @@ fetch('schools.json')
     .then(response => response.json())
     .then(data => {
         schoolData = data;
+        console.log(schoolData); // Проверяем структуру данных
         loadSchools(2024); // Начальный год
     })
     .catch(error => console.error('Ошибка загрузки данных:', error));
